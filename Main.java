09c211fc26ca0096b5da56bcde4f833e0decf3a5
Adithyasankar74 <112875396+Adithyasankar74@users.noreply.github.com>
2022-11-11 13:55:48 +0530

    Updated

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+  
+      int x =5;
+      int y =10;
+      int sum =  x + y;
+      System.out.println("The sum is " + sum);
+    }
+  }
+  
