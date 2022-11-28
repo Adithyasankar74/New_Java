b30cb6540c009c4422f44c25fa193e8d4cde7baf
Adithyasankar74 <112875396+Adithyasankar74@users.noreply.github.com>
2022-11-28 21:24:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
+import java.util.*;
+
 public class Main{
+  public static void main(String[]args){
+    System.out.println("Enter the size of array");
+    Scanner sc = new Scanner(System.in);
+    int size= sc.nextInt();
+    int number [] = new int [size];
+    System.out.println("Enter the elements in the array=");
 
-  public static void main(String[]argr){
+    for(int i = 0;i<size;i++){
 
-    int randomNum = (int)(Math.random() * 101); 
-    System.out.println(randomNum);
+      number[i] =sc.nextInt();
 
+    }
+
+    int max = number[0];
+    for(int i = 0;i<size;i++){
+
+      if(max<number[i]){
+        max = number[i];
+      }
+
+    }
+
+    System.out.print(max); 
   }
 }
